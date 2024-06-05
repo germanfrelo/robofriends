67e2126e4998f6f3914e3bf8ea78077bbec2e729
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-06-05 08:03:50 +0200

    Run Prettier after config change (#14)

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -5,13 +5,20 @@ import "./CardList.css";
 function CardList({ robots }) {
 	const listCards = robots.map((user, i) => (
 		<li key={user.id}>
-			<Card id={user.id} name={user.name} email={user.email} />
+			<Card
+				id={user.id}
+				name={user.name}
+				email={user.email}
+			/>
 		</li>
 	));
 
 	return (
 		// eslint-disable-next-line jsx-a11y/no-redundant-roles
-		<ul role="list" className="CardList">
+		<ul
+			role="list"
+			className="CardList"
+		>
 			{listCards}
 		</ul>
 	);
